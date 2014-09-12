f531dc3e6abf88424489eb5cd9ad5e176555a909
Siyamed Sinir <siyamed@wanelo.com>
2014-09-12 00:24:00 -0700

    change sample app package

--- sample/src/main/java/com/github/siyamed/shapeimageview/sample/SampleActivity.java
+++ sample/src/main/java/com/github/siyamed/shapeimageview/sample/SampleActivity.java
@@ -1,6 +1,5 @@
-package com.github.siyamed.shapeimageview;
+package com.github.siyamed.shapeimageview.sample;
 
-import android.com.ext.circular_imageview.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
