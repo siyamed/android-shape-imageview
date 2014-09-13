f71e0f621ba5ad2bfa0cb391878996e5f4f90368
Siyamed Sinir <siyamed@wanelo.com>
2014-09-13 03:16:26 -0700

    border test

--- library/src/main/java/com/github/siyamed/shapeimageview/SvgShader.java
+++ library/src/main/java/com/github/siyamed/shapeimageview/SvgShader.java
@@ -13,6 +13,7 @@ public class SvgShader extends ShaderHelper {
     private Path shapePath;
 
     private Path path = new Path();
+
     private Matrix pathMatrix = new Matrix();
     private float[] pathDimensions = new float[2];
 
@@ -22,7 +23,6 @@ public class SvgShader extends ShaderHelper {
     @Override
     public void init(Context context, AttributeSet attrs, int defStyle) {
         super.init(context, attrs, defStyle);
-        borderWidth = 0;
         int resId = -1;
         if(attrs != null){
             TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ShaderImageView, defStyle, 0);
@@ -76,12 +76,26 @@ public class SvgShader extends ShaderHelper {
                 pathDimensions[1] = (float) SvgUtil.HEART_PATH_HEIGHT;
 
                 pathMatrix.reset();
+
                 scale = Math.min(width / pathDimensions[0], height / pathDimensions[1]);
                 translateX = Math.round((width - pathDimensions[0] * scale) * 0.5f);
                 translateY = Math.round((height- pathDimensions[1] * scale) * 0.5f);
                 pathMatrix.setScale(scale, scale);
                 pathMatrix.postTranslate(translateX, translateY);
                 shapePath.transform(pathMatrix, path);
+                path.offset(borderWidth, borderWidth);
+
+//                if(borderWidth > 0) {
+//                    path.offset(borderWidth, borderWidth);
+//                    pathMatrix.reset();
+//                    scale = Math.min(viewWidth / pathDimensions[0], viewHeight / pathDimensions[1]);
+//                    translateX = Math.round((viewWidth - pathDimensions[0] * scale) * 0.5f);
+//                    translateY = Math.round((viewHeight- pathDimensions[1] * scale) * 0.5f);
+//                    pathMatrix.setScale(scale, scale);
+//                    pathMatrix.postTranslate(translateX, translateY);
+//                    shapePath.transform(pathMatrix, borderPath);
+//                    borderPath.op(path, Path.Op.DIFFERENCE);
+//                }
 
                 pathMatrix.reset();
                 matrix.invert(pathMatrix);
